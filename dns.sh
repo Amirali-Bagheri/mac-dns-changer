c8dc8e6f4c60cc64d9e985cf3b9affc923369112
Amirali Bagheri <76995556+Amirali-Bagheri@users.noreply.github.com>
2024-05-24 21:54:17 +0330

    Improve Script and Add DNS finder and fix bugs

--- dns.sh
+++ dns.sh
@@ -1,109 +1,253 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Define colors
-RED='\033[91m'
 GREEN='\033[92m'
-GREEN_solid='\033[42m'
-CYAN='\033[96m'
 NC='\033[0m'
 
 # Display options
-echo "${GREEN_solid}\nChoose an option to change your DNS server:${NC}\c"
+printf "${GREEN}\nChoose an option to change your DNS server:${NC}"
+
 
 # Define DNS servers array
 dns_servers=(
-    '{"number":1, "title":"Shecan", "dns":"178.22.122.100 185.51.200.2"}'
-    '{"number":2, "title":"403.online", "dns":"10.202.10.202 10.202.10.102"}'
-    '{"number":3, "title":"Electro", "dns":"78.157.42.100 78.157.42.101"}'
-    '{"number":4, "title":"Radar Game", "dns":"10.202.10.10 10.202.10.11"}'
-    '{"number":5, "title":"Server.ir", "dns":"194.104.158.48 194.104.158.78"}'
-    '{"number":6, "title":"Host Iran", "dns":"172.29.0.100 172.29.2.100"}'
-    '{"number":7, "title":"DNSPro.ir", "dns":"185.105.236.236 185.105.238.238"}'
-    '{"number":8, "title":"Begzar", "dns":"185.55.226.26 185.55.225.25"}'
-    '{"number":9, "title":"Shatel", "dns":"85.15.1.15 85.15.1.14"}'
-    '{"number":10, "title":"Asia Tech", "dns":"194.36.174.161 178.22.122.100"}'
-    '{"number":11, "title":"Pars Online", "dns":"91.99.101.12"}'
-    '{"number":12, "title":"Pishgaman", "dns":"5.202.100.101"}'
-    '{"number":13, "title":"Resaneh Pardaz", "dns":"185.186.242.161"}'
-    '{"number":20, "title":"Cloudflare", "dns":"1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001"}'
-    '{"number":21, "title":"Open DNS", "dns":"208.67.222.222 208.67.220.220 2620:119:35::35 2620:119:53::53"}'
-    '{"number":22, "title":"Google", "dns":"8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::8844"}'
-    '{"number":23, "title":"Quad9", "dns":"9.9.9.9 149.112.112.112 2620:fe::fe 2620:fe::9"}'
-    '{"number":24, "title":"PiHole", "dns":"192.168.100.27 fd5c:c307:7993:db00:2e0:4cff:fe6b:1f4"}'
-    '{"number":25, "title":"Some DNS", "dns":"91.239.100.100 89.233.43.71"}'
-    '{"number":26, "title":"CZ_NIC", "dns":"193.17.47.1 185.43.135.1"}'
-    '{"number":27, "title":"Level 3", "dns":"209.244.0.3 209.244.0.4"}'
-    '{"number":28, "title":"Comodo Group", "dns":"8.26.56.26 8.20.247.20"}'
-    '{"number":29, "title":"Control D", "dns":"76.76.2.0 76.76.10.0 2606:1a40:: 2606:1a40:1::"}'
-    '{"number":30, "title":"Alternative", "dns":"76.76.19.19 76.223.122.150 2602:fcbc::ad 2602:fcbc:2::ad"}'
-    '{"number":31, "title":"Versign", "dns":"64.6.64.6 64.6.65.6"}'
-    '{"number":32, "title":"Open Nic", "dns":"216.87.84.211 23.90.4.6"}'
-    '{"number":33, "title":"Yandex", "dns":"77.88.8.8 77.88.8.1"}'
-    '{"number":34, "title":"Dns Watch", "dns":"84.200.69.80 84.200.70.40"}'
-    '{"number":35, "title":"Oracle Dyn", "dns":"216.146.35.35 216.146.36.36"}'
-    '{"number":36, "title":"Freenom World", "dns":"80.80.80.80 80.80.81.81"}'
-    '{"number":37, "title":"Flash Start", "dns":"185.236.104.104 185.236.105.105"}'
-    '{"number":38, "title":"Neustar DNS", "dns":"156.154.70.5 156.154.71.5"}'
-    '{"number":39, "title":"DYN", "dns":"216.146.35.35 216.146.36.36"}'
+  "1|Shecan|178.22.122.100 185.51.200.2"
+  "2|403.online|10.202.10.202 10.202.10.102"
+  "3|Electro|78.157.42.100 78.157.42.101"
+  "4|Radar Game|10.202.10.10 10.202.10.11"
+  "5|Server.ir|194.104.158.48 194.104.158.78"
+  "6|Host Iran|172.29.0.100 172.29.2.100"
+  "7|DNSPro.ir|185.105.236.236 185.105.238.238"
+  "8|Begzar|185.55.226.26 185.55.225.25"
+  "9|Shatel|85.15.1.15 85.15.1.14"
+  "10|Asia Tech|194.36.174.161 178.22.122.100"
+  "11|Pars Online|91.99.101.12"
+  "12|Pishgaman|5.202.100.101"
+  "13|Resaneh Pardaz|185.186.242.161"
+  "20|Cloudflare|1.1.1.1 1.0.0.1"
+  "21|Open DNS|208.67.222.222 208.67.220.220"
+  "22|Google|8.8.8.8 8.8.4.4"
+  "23|Quad9|9.9.9.9 149.112.112.112"
+  "24|Some DNS|91.239.100.100 89.233.43.71"
+  "25|CZ_NIC|193.17.47.1 185.43.135.1"
+  "26|Level 3|209.244.0.3 209.244.0.4"
+  "27|Comodo Group|8.26.56.26 8.20.247.20"
+  "28|Control D|76.76.2.0 76.76.10.0"
+  "29|Alternative|76.76.19.19 76.223.122.150"
+  "30|Versign|64.6.64.6 64.6.65.6"
+  "31|Open Nic|216.87.84.211 23.90.4.6"
+  "32|Yandex|77.88.8.8 77.88.8.1"
+  "33|Dns Watch|84.200.69.80 84.200.70.40"
+  "34|Oracle Dyn|216.146.35.35 216.146.36.36"
+  "35|Freenom World|80.80.80.80 80.80.81.81"
+  "36|Flash Start|185.236.104.104 185.236.105.105"
+  "37|Neustar DNS|156.154.70.5 156.154.71.5"
+  "38|DYN|216.146.35.35 216.146.36.36"
 )
 
-# Print newline
-printf "\n"
-count=0
+# Function to print formatted options
+print_options() {
+    for dns_server in "${dns_servers[@]}"; do
+        IFS='|' read -r number title dns_list <<< "$dns_server"
+        echo "$number) $title ($dns_list)"
+    done
+}
 
-# Loop through DNS servers array and display options
-for dns_server in "${dns_servers[@]}"; do
-    ((count++)) # Increment count
-
-    # Parse JSON string and extract fields
-    number=$(jq -r '.number' <<< "$dns_server")
-    title=$(jq -r '.title' <<< "$dns_server")
-
-    # Output the fields with space between objects
-    printf "$number. $title"
-
-    # Add newline every 3 items to break into a new row
-    if ((count % 3 == 0)); then
-        printf "\n"
+# Function to calculate average ping time for a given DNS server
+get_average_ping() {
+    local dns=$1
+    avg=$(ping -c 4 -q "$dns" | tail -1 | awk -F'/' '{print $5}')
+    if [ -z "$avg" ]; then
+        echo "9999"
     else
-        printf "%-20s" "" # Add spacing between two items
+        echo "$avg"
     fi
-done
+}
 
-# Display custom and reset options
-printf '0. Custom DNS           101. Reset DNS\n\nEnter Number: '
-read dns_choose
+# Check if an argument was provided and set selection accordingly
+if [ $# -gt 0 ]; then
+    selection=$1
+else
+    read -p "
 
-# Get selected DNS
-dns=$(jq -r '.dns' <<< "${dns_servers[$((dns_choose-1))]}")
+1) Find Best Server (Auto) for 403 Unlock
+2) Show List (Manual Choose)
+3) Find Best Server (Auto) for All DNS Servers
+4) Reset DNS
+5) Check Current DNS
+6) Feedback
 
-echo "--------------------------------------------------------------------"
-
-# Custom DNS
-if [ "$dns_choose" -eq "0" ]; then
-    printf 'Enter a desired DNS server: '
-    read DNS
-    networksetup -setdnsservers Wi-Fi $DNS
+Enter your choice: " selection
 fi
 
-echo "[✓] ${RED}Setting up DNS...${NC}"
+if [ "$selection" == "1" ]; then
+    clear
+   # Auto: Calculate ping for selected DNS servers and select the best one
 
-# Check if var is set and within range
-if [[ -n $dns_choose && ${dns_servers[$dns_choose]+exists} ]]; then
-    # Set DNS servers
+   selected_servers=("Shecan" "403.online" "Google" "Cloudflare")
+   echo "Calculating ping for 403 Unlockers DNS servers..."
+   printf '\n'
+
+   pings=()
+   server_dns_map=()
+
+   for dns_server in "${dns_servers[@]}"; do
+       IFS='|' read -r number title dns_list <<< "$dns_server"
+       if [[ " ${selected_servers[*]} " =~ $title ]]; then
+           IFS=' ' read -r -a dns_array <<< "$dns_list"
+
+           dns_pings=()
+
+           for dns in "${dns_array[@]}"; do
+               ping=$(get_average_ping "$dns")
+               if [ "$ping" != "9999" ]; then
+                   dns_pings+=("$ping ms")
+                   pings+=("$ping|$dns|$title")
+               fi
+           done
+
+           # Store the DNS list for this server
+           server_dns_map+=("$title|$dns_list")
+
+           # Construct the final output with all pings
+           pings_str=$(IFS=', ' ; echo "${dns_pings[*]}")
+
+           echo "$title, Pings: ($pings_str)"
+       fi
+   done
+
+   # Determine the best DNS server by the lowest ping
+   if [ "${#pings[@]}" -gt 0 ]; then
+       best_ping_info=$(printf "%s\n" "${pings[@]}" | sort -n | head -n 1)
+       best_ping=$(echo "$best_ping_info" | cut -d'|' -f1)
+       best_dns=$(echo "$best_ping_info" | cut -d'|' -f2)
+       best_server=$(echo "$best_ping_info" | cut -d'|' -f3)
+
+       printf '\n'
+
+       echo "Best DNS server by lowest ping: $best_server with ping $best_ping ms"
+
+       # Find the DNS list for the best server
+       for server_dns in "${server_dns_map[@]}"; do
+           IFS='|' read -r server title_dns_list <<< "$server_dns"
+           if [ "$server" == "$best_server" ]; then
+               best_dns_list="$title_dns_list"
+               break
+           fi
+       done
+
+       printf '\n'
+
+       echo "Setting DNS servers to: $best_dns_list..."
+
+       # Set the DNS servers using networksetup
+       networksetup -setdnsservers Wi-Fi $best_dns_list
+   else
+       echo "No valid pings found."
+   fi
+
+
+    exit
+
+elif [ "$selection" == "2" ]; then
+    printf "\n\n${GREEN}Choose an option to change your DNS server:\n\n${NC}"
+    print_options
+    printf '\n(0) Custom DNS \n\n'
+    read -p "Enter the number corresponding to your choice: " dns_choose
+
+    if [ "$dns_choose" -eq "0" ]; then
+        read -p "Enter the desired DNS server: " custom_dns
+        dns="$custom_dns"
+    else
+        dns=$(echo "${dns_servers[$((dns_choose-1))]}" | cut -d'|' -f3)
+    fi
+
+    echo "Setting DNS servers..."
     networksetup -setdnsservers Wi-Fi $dns
+    printf "\n\n${GREEN}DNS Setup Done.\n${NC}"
+
+    exit
+
+elif [ "$selection" == "3" ]; then
+    echo "Calculating ping for all DNS servers..."
+
+    pings=()
+       server_dns_map=()
+
+       for dns_server in "${dns_servers[@]}"; do
+           IFS='|' read -r number title dns_list <<< "$dns_server"
+                          IFS=' ' read -r -a dns_array <<< "$dns_list"
+
+                          dns_pings=()
+
+                          for dns in "${dns_array[@]}"; do
+                              ping=$(get_average_ping "$dns")
+                              if [ "$ping" != "9999" ]; then
+                                  dns_pings+=("$ping ms")
+                                  pings+=("$ping|$dns|$title")
+                              fi
+                          done
+
+                          # Store the DNS list for this server
+                          server_dns_map+=("$title|$dns_list")
+
+                          # Construct the final output with all pings
+                          pings_str=$(IFS=', ' ; echo "${dns_pings[*]}")
+
+                          echo "$title, Pings: ($pings_str)"
+       done
+
+       # Determine the best DNS server by the lowest ping
+       if [ "${#pings[@]}" -gt 0 ]; then
+           best_ping_info=$(printf "%s\n" "${pings[@]}" | sort -n | head -n 1)
+           best_ping=$(echo "$best_ping_info" | cut -d'|' -f1)
+           best_dns=$(echo "$best_ping_info" | cut -d'|' -f2)
+           best_server=$(echo "$best_ping_info" | cut -d'|' -f3)
+
+           printf '\n'
+
+           echo "Best DNS server by lowest ping: $best_server with ping $best_ping ms"
+
+           # Find the DNS list for the best server
+           for server_dns in "${server_dns_map[@]}"; do
+               IFS='|' read -r server title_dns_list <<< "$server_dns"
+               if [ "$server" == "$best_server" ]; then
+                   best_dns_list="$title_dns_list"
+                   break
+               fi
+           done
+
+           printf '\n'
+
+           echo "Setting DNS servers to: $best_dns_list..."
+
+           # Set the DNS servers using networksetup
+           networksetup -setdnsservers Wi-Fi $best_dns_list
+       else
+           echo "No valid pings found."
+       fi
+    exit
+
+elif [ "$selection" == "4" ]; then
+    echo "Resetting DNS..."
+    networksetup -setdnsservers Wi-Fi empty
+    echo "DNS servers are reset."
+    exit
+
+elif [ "$selection" == "5" ]; then
+    echo "Current DNS servers:"
+    networksetup -getdnsservers Wi-Fi
+    exit
+
+elif [ "$selection" == "6" ]; then
+    echo "Contact me:"
+    echo "Email: bagheriamirali2000@gmail.com"
+    echo "GitHub: amirali-bagheri"
+    exit
+
 else
-    echo "Invalid selection or variable is not set."
+    echo "Invalid selection. Exiting..."
+    exit 1
 fi
 
 echo "[✓] ${GREEN}DNS Setup Done. ${NC}"
-
-# Reset DNS
-if [ "$dns_choose" -eq "101" ]; then
-    echo "[✓] ${RED}Removing${NC} these DNS servers:\n${CYAN}$(networksetup -getdnsservers Wi-Fi)${NC}"
-    sleep 0.5
-    networksetup -setdnsservers Wi-Fi empty
-    echo "${RED}[!]${NC} DNS servers are reset to your DHCP."
-fi
-
 sudo -k # Signs out of root
